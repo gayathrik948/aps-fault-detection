c0d39867339ee0177d20403d6835a94529c34f00
gayathrik948 <gayathrik948@gmail.com>
2022-12-09 17:51:11 +0530

    requirements.txt added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+def get_requirements():
+    pa
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="gayathri",
+    author_email="gayathrik948@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
+
